30831b0630ef0670065737f576576922a4186d36
Phil Hunt <philiphuntiv2@gmail.com>
2025-12-27 12:24:43 -0500

    Create update_env.ps1

---
+++ update_env.ps1
@@ -0,0 +1,15 @@
+
+# Get the current Path
+$Path = [Environment]::GetEnvironmentVariable("Path", "Machine")
+
+# Add Android Platform Tools to Path
+$Path += ";C:\Users\$env:Username\AppData\Local\android\Sdk\platform-tools"
+
+# Add Java Binaries to path
+$Path += ";C:\Program Files\Java\jdk-14\bin"
+
+# Save the updated Path
+[Environment]::SetEnvironmentVariable("Path", $Path, "Machine")
+
+# Set the 'JAVA_HOME' variable
+[Environment]::SetEnvironmentVariable("JAVA_HOME", ";C:\Program Files\Java\jdk-14", "Machine")
