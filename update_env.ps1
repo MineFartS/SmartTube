a32ddb8dee359e423d1c4ffa80a3b3bb8589f878
Phil Hunt <philiphuntiv2@gmail.com>
2026-02-05 16:32:56 -0500

    Update update_env.ps1

--- update_env.ps1
+++ update_env.ps1
@@ -1,15 +1,17 @@
 
 # Get the current Path
-$Path = [Environment]::GetEnvironmentVariable("Path", "Machine")
+$Path = [Environment]::GetEnvironmentVariable("Path", "User")
 
-# Add Android Platform Tools to Path
-$Path += ";C:\Users\$env:Username\AppData\Local\android\Sdk\platform-tools"
+$Dir = "C:\Users\$env:Username\AppData\Local\android\Sdk\platform-tools"
 
-# Add Java Binaries to path
-$Path += ";C:\Program Files\Java\jdk-14\bin"
+if ($Path.Contains($Dir)) {
 
-# Save the updated Path
-[Environment]::SetEnvironmentVariable("Path", $Path, "Machine")
+    Write-Host 'Android Platform Tools already in Path'
 
-# Set the 'JAVA_HOME' variable
-[Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Java\jdk-14", "Machine")
+} else {
+
+    [Environment]::SetEnvironmentVariable("Path", "$Path;$Dir", "User")
+
+    Write-Host 'Android Platform Tools added to Path'
+
+}
