b473d4466e48a90631fd48651353d8214534c767
Phil Hunt <philiphuntiv2@gmail.com>
2026-02-09 08:56:27 -0500

    Moved OverrideSettings to SidebarService.java

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/service/SidebarService.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/service/SidebarService.java
@@ -17,7 +17,116 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.liskovsoft.smartyoutubetv2.common.app.OverrideSettings;
+import com.liskovsoft.smartyoutubetv2.common.app.presenters.BrowsePresenter;
+import com.liskovsoft.smartyoutubetv2.common.misc.MediaServiceManager;
+import com.liskovsoft.youtubeapi.service.internal.MediaServiceData;
+import com.liskovsoft.smartyoutubetv2.common.prefs.PlayerData;
+
+import android.content.Context;
+
+import java.util.LinkedList;
+import java.lang.Integer;
+
+
+class OverrideSettings {
+
+    public void Run(Context context) {
+
+        Update_Notifications(context);
+
+        Hide_Content(context);
+
+        Hide_Sidebar_Tabs(context);
+
+        Highest_Buffer(context);
+
+    }
+
+    private void Update_Notifications(Context context) {
+
+        GeneralData GD = GeneralData.instance(context);
+
+        GD.setOldUpdateNotificationsEnabled(true);
+
+    }
+
+    private void Hide_Content(Context context) {
+
+        MediaServiceData MSD = MediaServiceData.instance();
+
+        LinkedList<Integer> content_types = new LinkedList<Integer>();
+
+        content_types.add(MediaServiceData.CONTENT_MIXES);
+
+        content_types.add(MediaServiceData.CONTENT_WATCHED_HOME);
+
+        content_types.add(MediaServiceData.CONTENT_SHORTS_CHANNEL);
+
+        content_types.add(MediaServiceData.CONTENT_SHORTS_HISTORY);
+
+        content_types.add(MediaServiceData.CONTENT_SHORTS_HOME);
+
+        content_types.add(MediaServiceData.CONTENT_SHORTS_SEARCH);
+
+        content_types.add(MediaServiceData.CONTENT_SHORTS_SUBSCRIPTIONS);
+
+        content_types.add(MediaServiceData.CONTENT_SHORTS_TRENDING);
+
+        for (int type : content_types) {
+
+            MSD.setContentHidden(type, true);
+
+        }
+
+    }
+
+    private void Hide_Sidebar_Tabs(Context context) {
+
+        BrowsePresenter BP = BrowsePresenter.instance(context);
+
+        LinkedList<Integer> sections = new LinkedList<Integer>();
+
+        sections.add(MediaGroup.TYPE_SHORTS);
+
+        sections.add(MediaGroup.TYPE_TRENDING);
+
+        sections.add(MediaGroup.TYPE_KIDS_HOME);
+
+        sections.add(MediaGroup.TYPE_SPORTS);
+
+        sections.add(MediaGroup.TYPE_LIVE);
+
+        sections.add(MediaGroup.TYPE_GAMING);
+
+        sections.add(MediaGroup.TYPE_NEWS);
+
+        sections.add(MediaGroup.TYPE_MUSIC);
+
+        sections.add(MediaGroup.TYPE_CHANNEL_UPLOADS);
+
+        sections.add(MediaGroup.TYPE_MY_VIDEOS);
+
+        sections.add(MediaGroup.TYPE_PLAYBACK_QUEUE);
+
+        //sections.add(MediaGroup.TYPE_SETTINGS);
+
+        for (Integer section : sections) {
+
+            BP.enableSection(0, false);
+
+        }
+
+    }
+
+    private void Highest_Buffer(Context context) {
+
+        PlayerData PD = PlayerData.instance(context);
+
+        PD.setVideoBufferType(PlayerData.BUFFER_HIGHEST);
+
+    }
+
+}
 
 public class SidebarService implements ProfileChangeListener {
 
@@ -40,8 +149,7 @@ public class SidebarService implements ProfileChangeListener {
 
         mContext = context.getApplicationContext();
 
-        OverrideSettings OR = new OverrideSettings();
-        OR.Run(context);
+        new OverrideSettings().Run(mContext);
 
         mPrefs = AppPrefs.instance(context);
         mPrefs.addListener(this);
@@ -144,9 +252,7 @@ public class SidebarService implements ProfileChangeListener {
     
     }
 
-    /**
-     * Contains sections and pinned items!
-     */
+    // Contains sections and pinned items!
     public boolean isSectionPinned(int sectionId) {
         
         Video section = Helpers.findFirst(
