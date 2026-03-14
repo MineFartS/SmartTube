df03d33440d669b70bd9b11554d3b3928458ed81
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-14 18:12:17 -0400

    Purged Old Tests

--- dependencies/MediaServiceCore/youtubeapi/src/main/java/com/liskovsoft/googlecommon/common/helpers/tests/ApiKeys.java
+++
@@ -1,8 +0,0 @@
-package com.liskovsoft.googlecommon.common.helpers.tests;
-
-public class ApiKeys {
-    public static final String RAW_AUTH_DATA_V1 = "";
-    public static final String RAW_JSON_AUTH_DATA_V2 =
-            "";
-    public static final String REFRESH_TOKEN = "";
-}
