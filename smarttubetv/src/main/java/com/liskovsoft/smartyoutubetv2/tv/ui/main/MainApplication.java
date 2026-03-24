eab6bad5780d397fb7861907d39ed7b29d369fb5
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-24 12:49:04 -0400

    Update MainApplication.java

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/main/MainApplication.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/main/MainApplication.java
@@ -22,43 +22,44 @@ import com.liskovsoft.smartyoutubetv2.tv.ui.search.tags.SearchTagsActivity;
 import com.liskovsoft.smartyoutubetv2.tv.ui.signin.SignInActivity;
 import com.liskovsoft.smartyoutubetv2.tv.ui.webbrowser.WebBrowserActivity;
 
-public class MainApplication
-    extends MultiDexApplication { // fix: Didn't find class
-                                  // "com.google.firebase.provider.FirebaseInitProvider"
-  static {
-    // fix youtube bandwidth throttling (best - false)???
-    // false is better for streams (less buffering)
-    System.setProperty("http.keepAlive", "false");
-    // fix ipv6 infinite video buffering???
-    // Better to remove this fix at all. Users complain about infinite loading.
-    // System.setProperty("java.net.preferIPv6Addresses", "true");
-    // Another IPv6 fix (no effect)
-    // https://stackoverflow.com/questions/1920623/sometimes-httpurlconnection-getinputstream-executes-too-slowly
-    // System.setProperty("java.net.preferIPv4Stack" , "true");
-  }
+public class MainApplication extends MultiDexApplication {
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    setupViewManager();
-  }
+    static {
 
-  private void setupViewManager() {
-    ViewManager viewManager = ViewManager.instance(this);
+        // fix youtube bandwidth throttling (best - false)???
+        // false is better for streams (less buffering)
+        System.setProperty("http.keepAlive", "false");
 
-    viewManager.setRoot(BrowseActivity.class);
-    viewManager.register(
-        SplashView.class, SplashActivity.class); // no parent, because it's root activity
-    viewManager.register(
-        BrowseView.class, BrowseActivity.class); // no parent, because it's root activity
-    viewManager.register(PlaybackView.class, PlaybackActivity.class, BrowseActivity.class);
-    viewManager.register(AppDialogView.class, AppDialogActivity.class, BrowseActivity.class);
-    viewManager.register(SearchView.class, SearchTagsActivity.class, BrowseActivity.class);
-    viewManager.register(SignInView.class, SignInActivity.class, BrowseActivity.class);
-    viewManager.register(AddDeviceView.class, AddDeviceActivity.class, BrowseActivity.class);
-    viewManager.register(ChannelView.class, ChannelActivity.class, BrowseActivity.class);
-    viewManager.register(
-        ChannelUploadsView.class, ChannelUploadsActivity.class, BrowseActivity.class);
-    viewManager.register(WebBrowserView.class, WebBrowserActivity.class, BrowseActivity.class);
-  }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setupViewManager();
+    }
+
+    private void setupViewManager() {
+
+        ViewManager viewManager = ViewManager.instance(this);
+
+        viewManager.setRoot(BrowseActivity.class);
+
+        viewManager.register(
+                SplashView.class,
+                SplashActivity.class); // no parent, because it's root activity
+
+        viewManager.register(
+                BrowseView.class,
+                BrowseActivity.class); // no parent, because it's root activity
+
+        viewManager.register(PlaybackView.class, PlaybackActivity.class, BrowseActivity.class);
+        viewManager.register(AppDialogView.class, AppDialogActivity.class, BrowseActivity.class);
+        viewManager.register(SearchView.class, SearchTagsActivity.class, BrowseActivity.class);
+        viewManager.register(SignInView.class, SignInActivity.class, BrowseActivity.class);
+        viewManager.register(AddDeviceView.class, AddDeviceActivity.class, BrowseActivity.class);
+        viewManager.register(ChannelView.class, ChannelActivity.class, BrowseActivity.class);
+        viewManager.register(
+                ChannelUploadsView.class, ChannelUploadsActivity.class, BrowseActivity.class);
+        viewManager.register(WebBrowserView.class, WebBrowserActivity.class, BrowseActivity.class);
+    }
 }
