3f4e3e73b1b2b30644d88f0506ec3424238e1217
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-26 19:34:08 -0400

    Remove Web Browser

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/main/MainApplication.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/main/MainApplication.java
@@ -15,7 +15,6 @@ import com.liskovsoft.smartyoutubetv2.common.app.views.SearchView;
 import com.liskovsoft.smartyoutubetv2.common.app.views.SignInView;
 import com.liskovsoft.smartyoutubetv2.common.app.views.SplashView;
 import com.liskovsoft.smartyoutubetv2.common.app.views.ViewManager;
-import com.liskovsoft.smartyoutubetv2.common.app.views.WebBrowserView;
 import com.liskovsoft.smartyoutubetv2.tv.ui.adddevice.AddDeviceActivity;
 import com.liskovsoft.smartyoutubetv2.tv.ui.browse.BrowseActivity;
 import com.liskovsoft.smartyoutubetv2.tv.ui.channel.ChannelActivity;
@@ -24,7 +23,6 @@ import com.liskovsoft.smartyoutubetv2.tv.ui.dialogs.AppDialogActivity;
 import com.liskovsoft.smartyoutubetv2.tv.ui.playback.PlaybackActivity;
 import com.liskovsoft.smartyoutubetv2.tv.ui.search.tags.SearchTagsActivity;
 import com.liskovsoft.smartyoutubetv2.tv.ui.signin.SignInActivity;
-import com.liskovsoft.smartyoutubetv2.tv.ui.webbrowser.WebBrowserActivity;
 
 import java.lang.Thread.UncaughtExceptionHandler;
 
@@ -48,19 +46,29 @@ public class MainApplication extends MultiDexApplication { // fix: Didn't find c
     }
 
     private void setupViewManager() {
+
         ViewManager viewManager = ViewManager.instance(this);
 
         viewManager.setRoot(BrowseActivity.class);
+
         viewManager.register(SplashView.class, SplashActivity.class); // no parent, because it's root activity
+
         viewManager.register(BrowseView.class, BrowseActivity.class); // no parent, because it's root activity
+
         viewManager.register(PlaybackView.class, PlaybackActivity.class, BrowseActivity.class);
+
         viewManager.register(AppDialogView.class, AppDialogActivity.class, BrowseActivity.class);
+
         viewManager.register(SearchView.class, SearchTagsActivity.class, BrowseActivity.class);
+
         viewManager.register(SignInView.class, SignInActivity.class, BrowseActivity.class);
+
         viewManager.register(AddDeviceView.class, AddDeviceActivity.class, BrowseActivity.class);
+
         viewManager.register(ChannelView.class, ChannelActivity.class, BrowseActivity.class);
+
         viewManager.register(ChannelUploadsView.class, ChannelUploadsActivity.class, BrowseActivity.class);
-        viewManager.register(WebBrowserView.class, WebBrowserActivity.class, BrowseActivity.class);
+
     }
 
 }
