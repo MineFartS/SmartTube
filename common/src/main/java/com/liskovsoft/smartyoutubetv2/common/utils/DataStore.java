181e0bade64965eee36a38e5c640f8e9736bed36
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-28 09:50:08 -0400

    Serialize DataStore

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/utils/DataStore.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/utils/DataStore.java
@@ -1,137 +1,72 @@
 package com.liskovsoft.smartyoutubetv2.common.utils;
 
+import com.liskovsoft.smartyoutubetv2.common.utils.Serializer;
 import com.liskovsoft.sharedutils.helpers.Helpers;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
+import java.io.Serializable;
 
 import javax.annotation.Nullable;
 
-public class DataStore {
+public class DataStore extends Fragment {
 
     private SharedPreferences mPrefs;
 
     public DataStore(String name) {
-        mPrefs = getSharedPreferences(name, Context.MODE_PRIVATE);
-    }
 
-    private String[] get(Integer x) {
-        return mPrefs.getString(x.toString(), null);
-    }
-
-    //====================================
-
-    public String getStr(Integer x, String def) {
-        return Helpers.parseStr(get(x), def);
-    }
-
-    public String getStr(Integer x) {
-        return getStr(x, null);
+        mPrefs = getContext().getSharedPreferences(name, Context.MODE_PRIVATE);
+    
     }
 
     //====================================
+    // GET
 
-    public boolean getBool(Integer x, boolean def) {
-        return Helpers.parseBoolean(get(x), def);
-    }
-
-    public boolean getBool(Integer x) {
-        return getBool(x, null);
-    }
-
-    //====================================
-
-    public Integer getInt(Integer x, Integer def) {
-        return Helpers.parseInt(get(x), def);
-    }
-
-    public Integer getInt(Integer x) {
-        return getInt(x, null);
-    }
-
-    //====================================
-
-    public Map getMap(Integer x, Map def) {
-
-        Map<T, K> result = new HashMap<>();
-
-        if (spec != null) {
-
-            String[] listArr = splitArray(spec);
-
-            for (String item : listArr) {
-                //String[] keyValPair = item.split("\\|");
-                String[] keyValPair = split(item, PAIR_DELIM);
-
-                if (keyValPair.length != 2) {
-                    continue;
-                }
-
-                result.put(keyParser.parse(keyValPair[0]), valueParser.parse(keyValPair[1]));
-            }
-        }
-
-        return result;
-    }
-
-    //====================================
-
-    public <T> List<T> getList(
+    public <T> T get(
         Integer x, 
-        @Nullable List<T> def,
-        Helpers.Parser<T> parser
+        T def
     ) {
 
-        String[] raw = get(x);
-
-        List<T> result = new ArrayList<>();
+        String key = x.toString();
+        
+        String raw = mPrefs.getString(key, null);
 
         if (raw == null) {
-
             return def;
-
         } else {
-
-            String[] listArr = Helpers.split(raw, Helpers.ARRAY_DELIM);
-
-            for (String item : listArr) {
-
-                T parsed = parser.parse(item);
-
-                if (parsed != null) {
-                    result.add(parsed);
-                }
-
-            }
-
+            return (T) Serializer.decode(raw);
         }
-
-        return result;
+    
     }
 
-    public List<String> getStrList(Integer x, List<String> def) {
-        getList(x, def, Helpers::parseStr);
-    }
-
-    public List<String> getStrList(Integer x) {
-        return getStrList(x, null);
+    public <T> T get(
+        Integer x
+    ) {
+        return get(x, null);
     }
 
     //====================================
+    // PUT
 
-    public void put(Integer x, Object value) {
+    public void put(
+        Integer x, 
+        Serializable value
+    ) {
 
         String key = x.toString();
-        String val = value.toString();
+        String val = Serializer.encode(value);
 
         mPrefs.edit()
             .putString(key, val)
             .apply();
 
     }
+
+    //====================================
 
 }
