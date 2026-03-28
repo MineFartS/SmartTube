e0cf917343de8b30f7cd73d8878a43defa32b15c
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-28 19:31:52 -0400

    Move "gradle-wrapper.jar"

--- build.ps1
+++ build.ps1
@@ -8,6 +8,6 @@ Clear-Host
 
 # Execute Gradle
 & $JAVA `
-    '-classpath' ".\gradle\wrapper\gradle-wrapper.jar" `
+    '-classpath' ".\pkg\gradle-wrapper.jar" `
     'org.gradle.wrapper.GradleWrapperMain' `
     "clean" "installStstableDebug"
