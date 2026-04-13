53bed8b0af7c659fad1f4077ae5206f2fbe40746
Phil Hunt <philiphuntiv2@gmail.com>
2026-04-13 06:37:30 -0400

    Remove header comments

--- pkg/j2v8/src/main/java/com/eclipsesource/v8/PlatformDetector.java
+++ pkg/j2v8/src/main/java/com/eclipsesource/v8/PlatformDetector.java
@@ -1,28 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2017 EclipseSource and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Trustin Lee - original OS/Arch/Vendor detection code (see: https://github.com/trustin/os-maven-plugin)
- *    Wolfgang Steiner - initial API and implementation
- *
- * Copyright 2014 Trustin Heuiseung Lee.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
 package com.eclipsesource.v8;
 
 import java.io.BufferedReader;
--- pkg/j2v8/src/main/java/com/eclipsesource/v8/utils/ConcurrentV8.java
+++ pkg/j2v8/src/main/java/com/eclipsesource/v8/utils/ConcurrentV8.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2016 Brandon Sanders
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Brandon Sanders - initial API and implementation and/or initial documentation
- ******************************************************************************/
 package com.eclipsesource.v8.utils;
 
 import com.eclipsesource.v8.V8;
