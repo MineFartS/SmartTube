11914e8381c5aa0822cb4aff5862b1fda747d92a
Phil Hunt <philiphuntiv2@gmail.com>
2026-06-06 21:23:15 -0400

    Fix MalformedJsonException

--- src/main/java/minefarts/smarttube/google/common/converters/gson/GsonResponseBodyConverter.java
+++ src/main/java/minefarts/smarttube/google/common/converters/gson/GsonResponseBodyConverter.java
@@ -30,7 +30,17 @@ final class GsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
     //JsonReader jsonReader = gson.newJsonReader(new InputStreamReader(newStream));
 
     JsonReader jsonReader = gson.newJsonReader(value.charStream());
+    jsonReader.setLenient(true);
     try {
+      // Some endpoints occasionally return HTML/error text instead of JSON.
+      // Gson leniency won't help if payload doesn't start like JSON.
+      // Attempt a non-fatal peek to ensure we don't get stuck.
+      try {
+        jsonReader.peek();
+      } catch (Exception ignored) {
+        // fall through and let adapter handle it
+      }
+
       T result = adapter.read(jsonReader);
 
       // Dumping all data for debug purposes
