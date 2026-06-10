c77c48d25c2b990aeabbcd47b6be3c0e42aa2949
Phil Hunt <philiphuntiv2@gmail.com>
2026-06-10 14:34:13 -0400

    Move submodules to /lib/

--- gradlew.bat
+++ gradlew.bat
@@ -22,24 +22,11 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
-set JAVA_HOME=%APP_HOME%jdk
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set JAVA_EXE=%APP_HOME%/lib/jdk17/bin/java.exe
 
-if exist "%JAVA_EXE%" goto execute
-
-echo. 1>&2
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
-echo. 1>&2
-echo Please set the JAVA_HOME variable in your environment to match the 1>&2
-echo location of your Java installation. 1>&2
-
-goto fail
-
-:execute
 @rem Setup the command line
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
