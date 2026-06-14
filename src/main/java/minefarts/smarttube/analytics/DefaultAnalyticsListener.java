12261039b755479730fa95e85d5e911a6490052d
Phil Hunt <philiphuntiv2@gmail.com>
2026-06-14 05:44:34 -0400

    Remove Analytics

--- src/main/java/minefarts/smarttube/analytics/DefaultAnalyticsListener.java
+++
@@ -1,8 +0,0 @@
-package minefarts.smarttube.analytics;
-
-/**
- * @deprecated Use {@link AnalyticsListener} directly for selective overrides as all methods are
- *     implemented as no-op default methods.
- */
-@Deprecated
-public abstract class DefaultAnalyticsListener implements AnalyticsListener {}
