f40912a239b7329c6a3923b43724dd5c1a3a12a4
Phil Hunt <philiphuntiv2@gmail.com>
2026-06-17 07:10:36 -0400

    Purge Dead Code

--- src/main/java/minefarts/smarttube/app/models/data/DislikesResult.java
+++ src/main/java/minefarts/smarttube/app/models/data/DislikesResult.java
@@ -7,31 +7,14 @@ import minefarts.smarttube.google.common.converters.jsonpath.JsonPath;
 
 public class DislikesResult {
 
-    @JsonPath("$.id")
-    private String id;
-
-    @JsonPath("$.dateCreated")
-    private String dateCreated;
-    
     @JsonPath("$.likes")
     private Integer likes;
     
     @JsonPath("$.dislikes")
     private Integer dislikes;
     
-    @JsonPath("$.rating")
-    private Integer rating;
-    
     @JsonPath("$.viewCount")
     private Long viewCount;
-    
-    @JsonPath("$.deleted")
-    private Boolean deleted;
-        
-    @Nullable
-    public String getVideoId() {
-        return id;
-    }
 
     public long getViewCount() {
         return viewCount != null ? viewCount : 0L;
