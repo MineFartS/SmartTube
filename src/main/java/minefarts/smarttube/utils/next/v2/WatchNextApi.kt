aee3b3e6ca150bac178bc2f1e34149cbbb22b17a
Phil Hunt <philiphuntiv2@gmail.com>
2026-06-21 18:20:44 -0400

    Update WatchNextApi.getWatchNextResult()

--- src/main/java/minefarts/smarttube/utils/next/v2/WatchNextApi.kt
+++ src/main/java/minefarts/smarttube/utils/next/v2/WatchNextApi.kt
@@ -4,8 +4,11 @@ import minefarts.smarttube.google.common.converters.gson.WithGson
 import minefarts.smarttube.utils.next.v2.gen.UnlocalizedTitleResult
 import minefarts.smarttube.utils.next.v2.gen.WatchNextResult
 import minefarts.smarttube.utils.next.v2.gen.WatchNextResultContinuation
+
 import retrofit2.Call
 import retrofit2.http.*
+
+import okhttp3.RequestBody
 
 @WithGson
 internal interface WatchNextApi {
@@ -17,7 +20,7 @@ internal interface WatchNextApi {
     @Headers("Content-Type: application/json")
     @POST("https://www.youtube.com/youtubei/v1/next")
     fun getWatchNextResult(
-        @Body watchNextQuery: String, 
+        @Body watchNextQuery: RequestBody,
         @Header("X-Goog-Visitor-Id") visitorId: String
     ): Call<WatchNextResult?>
 
