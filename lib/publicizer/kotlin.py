7301872edc4b1dbdff73a4ad915d7dac77b54630
Phil Hunt <philiphuntiv2@gmail.com>
2026-07-19 10:54:52 -0400

    Simplify run.py

--- lib/publicizer/kotlin.py
+++
@@ -1,8 +0,0 @@
-import re
-
-# Kotlin Regex Patterns
-# Matches internal, private, or protected visibility modifiers
-KOTLIN_MODIFIERS = re.compile(r'\b(internal|private|protected)\b')
-
-def rewrite(content: str) -> str:
-    return KOTLIN_MODIFIERS.sub('public', content)
--- lib/publicizer/run.py
+++ lib/publicizer/run.py
@@ -2,27 +2,34 @@ from philh_myftp_biz.terminal import set_package
 from philh_myftp_biz.pc import loc, Path
 from philh_myftp_biz import VERBOSE
 from sys import argv
+import re
 
 VERBOSE.enable()
 
 set_package(loc.script)
 
+patterns = [
+    
+    # Classes
+    r'\b(private|protected|internal)(?=\s+(?:(?:abstract|sealed|data|enum|open|inner|final|synchronized)\s+)*(?:class|interface|object)\b)',
+    
+    # Functions
+    r'\b(private|protected|internal)(?=\s+(?:(?:synchronized|final|abstract|inline|external|tailrec|operator|infix)\s+)*(?:fun|void\s+\w+|[\w<>\[\]]+\s+\w+(?=\s*\()))(?!\s+(?:[^\{]*?\b(?:open|override)\b))'
+
+]
+
 for file in Path(argv[1]).descendants:
 
-    match file.ext:
-        case 'kt':
-            from .kotlin import rewrite
-        case 'java':
-            from .java import rewrite
-        case _:
-            continue
-    
+    if file.ext not in ['java', 'kt']:
+        continue
+ 
     print(f'Modifying:', file)
 
     with file.open() as f:
         content = f.read()
-    
-    content = rewrite(content)
+
+    for pat in patterns:
+        content = re.sub(pat, 'public', content)
 
     with file.open('w') as f:
         f.write(content)
