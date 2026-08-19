6330fffcb6101adf853f4f0b3fe2702d0eb8059b
Phil Hunt <philiphuntiv2@gmail.com>
2026-08-19 18:23:47 -0400

    Hide log entries using regex & tags

--- logs.ps1
+++ logs.ps1
@@ -1,17 +1,46 @@
-param(
-    [Switch] $Verbose
-)
-
 Import-Module "$PSScriptRoot/__mod__.psm1" -Force
 
 Invoke-ADB
 
 Clear-Host
 
-$Level = if ($Verbose) {"V"} else {"W"}
+$HidePats = @(
+    'fromNullable'
+    'concurrent copying GC'
+    'A resource failed to call close'
+    'TRACE Starting certificate trust'
+    'Accessing hidden (field|method)'
+)
+
+$HideTags = @(
+    'okhttp.OkHttpClient'
+    'ExoPlayerImpl'
+    'chatty'
+    'CameraManagerGlobal'
+    'AudioTrack'
+    'MediaCodecRenderer'
+    'AmazonKeyEventLogging'
+    'StaticLayout'
+    'cr_VAUtil'
+    'Chrome_InProcGp'
+    'IMGSRV'
+    'cr_AWVDeploymentClient'
+    'WebViewExtAmazonConfig'
+    'WebViewExtAmazon'
+    'VideoCapabilities'
+    'Choreographer'
+    'OpenGLRenderer'
+    'ConfigStore'
+    'ion'
+    'PerfStatsManager'
+    'MultiDex'
+    'libc'
+)
 
 Invoke-Python 'lib/pidcat/pidcat.py' `
     "minefarts.smarttube" `
-    '--min-level' $Level `
+    '--min-level' 'V' `
     '--sdk' $Env:ANDROID_SDK_ROOT `
+    '--regex' ('^(?!.*(' + ($HidePats -join '|') + '))') `
+    @($HideTags | ForEach-Object { '--ignore-tag', $_ }) `
     '--clear'
